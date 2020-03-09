46ab26679d7d382affbc36e5ff20407835e71a73
admin <admin@192.168.0.105>
2020-03-09 12:07:27 +0530

    Removed some space

--- Selenium2020/src/saif/alam/com/WriteDataOnExcel.java
+++ Selenium2020/src/saif/alam/com/WriteDataOnExcel.java
@@ -2,13 +2,11 @@ package saif.alam.com;
 
 import library.ExcelWriteDataConfig;
 
-
-
 public class WriteDataOnExcel {
 
 	public static void main(String[] args) throws Exception
 	{
-		//this is writing from config
+		//this is writing from config  
 		ExcelWriteDataConfig excelWrite = new ExcelWriteDataConfig("D:\\Study\\Excel Data\\TestData.xlsx");
 		String msg=excelWrite.setDataOnSheet(0, 2, 3);
         System.out.println(msg);		
