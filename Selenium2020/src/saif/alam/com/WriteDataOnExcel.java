63f10e7a5d89ed5287c786d4cef507490bd1f8fc
admin <admin@192.168.0.105>
2020-03-09 12:04:58 +0530

    We added some comments

--- Selenium2020/src/saif/alam/com/WriteDataOnExcel.java
+++ Selenium2020/src/saif/alam/com/WriteDataOnExcel.java
@@ -8,7 +8,7 @@ public class WriteDataOnExcel {
 
 	public static void main(String[] args) throws Exception
 	{
-		 
+		//this is writing from config
 		ExcelWriteDataConfig excelWrite = new ExcelWriteDataConfig("D:\\Study\\Excel Data\\TestData.xlsx");
 		String msg=excelWrite.setDataOnSheet(0, 2, 3);
         System.out.println(msg);		
