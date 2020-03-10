81c2885350ce1e59b7ba793fb39fd02d30587a5a
admin <admin@192.168.0.105>
2020-03-11 00:28:03 +0530

    We use log4j and property file

--- Selenium2020/src/saif/alam/com/WriteDataOnExcel.java
+++ Selenium2020/src/saif/alam/com/WriteDataOnExcel.java
@@ -1,15 +1,22 @@
 package saif.alam.com;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 import library.ExcelWriteDataConfig;
 
 public class WriteDataOnExcel {
 
 	public static void main(String[] args) throws Exception
 	{
+		Logger  logger = Logger.getLogger("WriteDataOnExcel");
+		PropertyConfigurator.configure("./PropertiesFile/log4j.properties");
 		//this is writing from config  
 		ExcelWriteDataConfig excelWrite = new ExcelWriteDataConfig("D:\\Study\\Excel Data\\TestData.xlsx");
+		logger.info("Load the excel file");
 		String msg=excelWrite.setDataOnSheet(0, 2, 3);
-        System.out.println(msg);		
+		logger.info("sheet will be update with given above info");
+		logger.info("Sheet data"+msg);		
 	}
 
 }
